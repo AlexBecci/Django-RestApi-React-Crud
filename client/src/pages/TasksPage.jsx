709cdbdf8d11cf830d14cbed78dddcfc79ba7e89
Alex Becci <alex.becci@bingopilar.corp>
2024-01-10 11:30:21 -0300

    get task, Back Axios get Tasks and TaskCard component

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import TasksList from '../components/TasksList'
 function TasksPage() {
-  return (
-    <div>TasksPage</div>
-  )
+    return (
+        <TasksList />
+    )
 }
 
 export default TasksPage
