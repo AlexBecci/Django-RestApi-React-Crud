8b0a6aff08d286078c554f935b7dbba7f5ac61ec
Alex Becci <alex.becci@bingopilar.corp>
2024-01-10 11:56:58 -0300

    create Tasks

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { useForm } from "react-hook-form";
+import { createTask } from "../api/tasks.api";
+import { useNavigate } from "react-router-dom"
 function TaskFormPage() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const navigate = useNavigate()
+  const onSubmit = handleSubmit(async data => {
+    await createTask(data)
+    navigate("/tasks") 
+  })
+
   return (
-    <div>TaskFormPage</div>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder='Title' {...register('title', { required: true })} />
+        {errors.title && <span>  title is required</span>}
+        <textarea rows="3" placeholder='Description' {...register('description', { required: true })}></textarea>
+        {errors.description && <span> password is required</span>}
+
+        <button>Save</button>
+      </form>
+    </div>
   )
 }
 
